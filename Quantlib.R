62d6b34e1b6914b2a1fcba30e7b312a52daffaa6
Matt <matt@firefinch.io>
2017-06-03 12:50:23 +0100

    Update

--- Quantlib.R
+++ Quantlib.R
@@ -1,4 +1,5 @@
 
+# Installs basic BraveRock packages from git source alongside dependencies
 installQuantPackages<-function()
 {
   #install.packages("roxygen2")
@@ -11,6 +12,7 @@ installQuantPackages<-function()
   install_github("braverock/PerformanceAnalytics")
 }
 
+#Loads basic libraries and sets up required environments for Quantstrat
 loadLibraries<-function()
 {
   require(quantmod)
@@ -18,6 +20,22 @@ loadLibraries<-function()
   library(readr)
   
   #Quantstrat boilerplate
+  
+  #TODO - review estra boilerplate from Harry Georgakopolous' code
   .blotter<-new.env()
   .strategy<-new.env()
 }
+
+#Returns list of known AIM tickers from AimIndex.csv
+#Full CSV returned from function call if needed
+# 2 Variants of symbols list (either as LON:XXX or XXX.L) returned to parent.env
+loadAIMSymbols<-function()
+{
+  setwd("./")
+  AimIndex <- read_csv("./AimIndex.csv")
+  LonSymbols<-AimIndex$Symbol
+  LSymbols<-AimIndex$Symbol2
+  return(AimIndex)
+}
+
+
